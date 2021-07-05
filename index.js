0ea4d62869a1f849fd9dd23c9b2e9553d8eac534
Sarfaraz <m.s.hussain@outlook.com>
2021-07-05 13:54:09 -0500

    added function for engineer questions

--- index.js
+++ index.js
@@ -2,7 +2,28 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 const Engineer = require("./lib/Engineer.js");
 
+function EngQs() {
+  inquirer
+    .prompt([
+      { type: "input", name: "name", message: "What is their name?" },
 
+      { type: "input", name: "ID", message: "What is their ID?" },
+
+      { type: "input", name: "email", message: "What is their E-mail?" },
+
+      {
+        type: "input",
+        name: "github",
+        message: "What is their Github Username?",
+      },
+    ])
+    .then(function (input) {
+      // console.log(input)
+      const Eng = new Engineer(input.name, input.ID, input.email, input.github);
+      console.log(Eng);
+      start()
+    });
+} 
 
 function start() {
   inquirer
