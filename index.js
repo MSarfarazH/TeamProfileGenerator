ee4acc7322b4e36a083a7e1fc2c77ab693f5a33d
Sarfaraz <m.s.hussain@outlook.com>
2021-07-05 13:53:24 -0500

    added inquirer function

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-const fs = require('fs'); 
-const inquirer = require('inquirer');
+const fs = require("fs");
+const inquirer = require("inquirer");
+const Engineer = require("./lib/Engineer.js");
 
+
+
+function start() {
+  inquirer
+    .prompt({
+      type: "list",
+      name: "role",
+      message: "what is the role of the team member?",
+      choices: ["Manager", "Engineer", "Intern", "Exit"],
+    })
+
+    .then(function (input) {
+      // console.log(input)
+      if (input.role === "Engineer") {
+        EngQs();
+      }
+    });
+}
+start();
