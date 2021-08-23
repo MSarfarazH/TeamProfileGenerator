10d7521b5b9f68b98a0af0cad4cd0a972dabe381
Sarfaraz <m.s.hussain@outlook.com>
2021-08-23 17:33:55 -0500

    fixed and completed

--- app.js
+++ app.js
@@ -9,9 +9,8 @@ const engArray = []
 const manArray = []
 const intArray = []
 
-
-
 console.log(HTMLGenerator)
+
 function EngQs() {
   inquirer.prompt([
       { type: "input", name: "name", message: "What is their name?" },
@@ -38,7 +37,6 @@ function EngQs() {
       // console.log(input)
       const Man = new Manager(input.name, input.ID, input.email, input.officenum);
       manArray.push(Man)
-      console.log(teamArray)
       start()
     });
 }
@@ -59,10 +57,9 @@ function IntQs() {
     });
 }
 
-function HTMLGenerator () {
-  GenerateManager(manArray)
-  console.log(GenerateManager(manArray))
-}
+// function generateHTML () {
+  
+// }
 
 function writeToFile(input) {
   fs.writeFile(input, (err) => {
@@ -92,12 +89,89 @@ function start() {
         IntQs();
       }
       if (input.role === "Exit") {
-        fs.writeFile("index.html", HTMLGenerator(input), function (err) {
-          if (err) throw err;
-          console.log("Saved!");
-        });
+        end()
       }
     });
 }
 
+function end() {
+  console.log(engArray)
+  console.log(manArray)
+  console.log(intArray)
+
+  const engInputArray = []
+  const manInputArray = []
+  const intInputArray = []
+  const allInputArray = []
+
+  engArray.forEach (function(input){
+    var engInput = HTMLGenerator.GenerateEngineer(input)
+
+    engInputArray.push(engInput)
+  })
+
+  manArray.forEach (function(input){
+    var manInput = HTMLGenerator.GenerateManager(input)
+
+    manInputArray.push(manInput)
+  })
+
+  intArray.forEach (function(input){
+    var intInput = HTMLGenerator.GenerateIntern(input)
+
+    intInputArray.push(intInput)
+  })
+
+  var joinedEngArray = engInputArray.join("")
+  var joinedManArray = manInputArray.join("")
+  var joinedIntArray = intInputArray.join("")
+
+  allInputArray.push(joinedEngArray, joinedIntArray, joinedManArray)
+  console.log(allInputArray.join(""))
+  
+  const generateTeamPage = function (joinedAllInputArray) {
+    var joinedAllInputArray = allInputArray.join("")
+  return `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Team Profile</title>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+      <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+      <header>
+          <nav class="navbar" id="navbar">
+              <span class="navbar-brand mb-0 h1 w-100 text-center" id="navbar-text">Team Profile</span>
+          </nav>
+      </header>
+      <main>
+          <div class="container">
+              <div class="row justify-content-center" id="team-cards">
+                  <!--Team Cards-->
+                  ${joinedAllInputArray}
+              </div>
+          </div>
+      </main>
+      
+  </body>
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  </html>
+`;
+};
+  
+console.log(generateTeamPage)
+
+fs.writeFileSync("./dist/index.html", generateTeamPage(), 'UTF8')
+  
+}
+
 start();
+
--- src/HTMLGenerator.js
+++ src/HTMLGenerator.js
@@ -1,39 +1,40 @@
-const GenerateManager = () => {
+
+const GenerateManager = (input) => {
    return `<div class="card" style="width: 18rem;">
     <div class="card-body">
       <h5 class="card-title">${input.name}</h5>
       <h5 class="card-title">Manager</h5>
     </div>
     <ul class="list-group list-group-flush">
-      <li class="list-group-item">ID:${input.ID} </li>
+      <li class="list-group-item">ID:${input.id} </li>
       <li class="list-group-item">Email:${input.email} </li>
-      <li class="list-group-item">Office:${input.officenum} </li>
+      <li class="list-group-item">Office:${input.officeNumber} </li>
     </ul>
   </div>`
 }
 
-const GenerateEngineer = () => {
+const GenerateEngineer = (input) => {
    return `<div class="card" style="width: 18rem;">
     <div class="card-body">
       <h5 class="card-title">${input.name}</h5>
       <h5 class="card-title">Engineer</h5>
     </div>
     <ul class="list-group list-group-flush">
-      <li class="list-group-item">ID:${input.ID} </li>
+      <li class="list-group-item">ID:${input.id} </li>
       <li class="list-group-item">Email:${input.email} </li>
       <li class="list-group-item">Github:${input.github} </li>
     </ul>
   </div>`
 }
 
-const GenerateIntern = () => {
+const GenerateIntern = (input) => {
    return `<div class="card" style="width: 18rem;">
     <div class="card-body">
       <h5 class="card-title">${input.name}</h5>
       <h5 class="card-title">Intern</h5>
     </div>
     <ul class="list-group list-group-flush">
-      <li class="list-group-item">ID:${input.ID} </li>
+      <li class="list-group-item">ID:${input.id} </li>
       <li class="list-group-item">Email:${input.email} </li>
       <li class="list-group-item">School:${input.school} </li>
     </ul>
@@ -41,4 +42,4 @@ const GenerateIntern = () => {
 }
 
 
-module.exports = 'HTMLGenerator.js';
+module.exports = {GenerateManager, GenerateEngineer, GenerateIntern};
